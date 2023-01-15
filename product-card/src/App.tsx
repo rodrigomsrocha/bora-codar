e60449db181de06ce97ac210792f7d170a593318
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-01-15 19:11:24 -0300

    product information section

--- product-card/src/App.tsx
+++ product-card/src/App.tsx
@@ -1,10 +1,12 @@
+import { Info } from "./components/Info";
 import { Preview } from "./components/Preview";
 
 function App() {
   return (
     <div className="min-h-screen w-full bg-bg-color flex items-center justify-center">
-      <div className="max-w-[688px] mx-auto px-4 flex items-center">
+      <div className="max-w-8xl mx-auto px-4 flex items-center">
         <Preview />
+        <Info />
       </div>
     </div>
   );
---
+++ product-card/src/components/Info/index.tsx
@@ -0,0 +1,16 @@
+export function Info() {
+  return (
+    <div className="text-primary">
+      <div className="flex flex-col gap-3 mb-5">
+        <span className="text-[10px] leading-none">CÓDIGO: 42404</span>
+        <h1 className="text-[32px] leading-none">Sofá Margot II - Rosé</h1>
+        <strong className="text-base text-secondary font-normal leading-none">
+          R$ 4.000
+        </strong>
+      </div>
+      <button className="px-4 py-2 border border-primary rounded-full">
+        ADICIONAR À CESTA
+      </button>
+    </div>
+  );
+}
